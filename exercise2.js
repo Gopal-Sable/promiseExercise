2aaea29d5127028d1e5a690168f7850dca7688d3
Gopal-Sable <74057505+Gopal-Sable@users.noreply.github.com>
2025-02-21 12:00:24 +0530

    Initial commit

---
+++ exercise2.js
@@ -0,0 +1,35 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+
+    5. Print out "Step 1 complete" when the first promise fulfills
+    6. Have the first promise return another new Promise that will
+       fulfill after 3 seconds with the message: "Step 2 Complete"
+
+    7. Print out the message from the second promise after it
+       fulfills ("Step 2 Complete")
+
+    HINT: Use setTimeout for the delay
+*/
+console.log("Program Started");
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    return resolve();
+  }, 3000);
+});
+
+console.log(promise);
+console.log("Program in progress...");
+promise
+  .then(() => console.log("Step 1 complete"))
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve("Step 2 Complete");
+      }, 3000);
+    });
+  })
+  .then((res) => console.log(res))
+  .catch(() => console.log("Program failure"));
---
+++ exercise4.js
@@ -0,0 +1,31 @@
+/*
+    1. Create a Promise that resolves with the number 10 after
+       3 seconds
+    2. Create another Promise that resolves with the number
+       20 after 5 seconds
+
+    How can we log out the sum (30) of these two resolved values
+    once, after BOTH promises successfully fulfill?
+
+    HINT: Use Google/Documentation to help find an answer
+    HINT2: You can Google for something like:
+           "resolve 2 promises at the same time javascript"
+*/
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 3000);
+});
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20);
+  }, 5000);
+});
+
+const getSum = (nums) => {
+  return nums.reduce((sum, val) => sum + val, 0);
+};
+Promise.all([promise1, promise2]).then((val) => {
+  console.log(getSum(val));
+});
